d6d0a2d828671cc174d2857a9394b2f3bbeb4a9a
gitgundam <335968838@qq.com>
2020-11-25 21:42:11 +0800

    deploy

---
+++ precache-manifest.c7cac3316e5dfd9a4f7974fcc21d8747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5073c7ca3b92926a024c",
+    "url": "/cash-app-website/css/app.361e3388.css"
+  },
+  {
+    "revision": "90d243b8f499fc713f5dad7608cee432",
+    "url": "/cash-app-website/index.html"
+  },
+  {
+    "revision": "5073c7ca3b92926a024c",
+    "url": "/cash-app-website/js/app.560181f3.js"
+  },
+  {
+    "revision": "e9687fa3fa4e5a3220c6",
+    "url": "/cash-app-website/js/chunk-vendors.bc1e88e2.js"
+  },
+  {
+    "revision": "946f873d2fdb2c82204d546147f9e7de",
+    "url": "/cash-app-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/cash-app-website/robots.txt"
+  }
+]);
